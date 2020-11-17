92dc71d110476736b134f1a3c191a02623147460
Sean-Briscoe <72442700+Sean-Briscoe@users.noreply.github.com>
2020-11-17 01:54:21 +0000

    UPDATE SOON

--- LCA.java
+++ LCA.java
@@ -2,29 +2,62 @@ import java.util.ArrayList;
 
 class Node { 
 	int data; 
-	ArrayList<Node> children; 
+	ArrayList<Node> parents;
+	int colour;  //Colour will be used to colour tree, 0 is red, 1 is blue, 2 is purple (where blue x red)
+	int counter; //will be used to decide lowest
 
 	Node(int value) { 
 		data = value; 
-		children = null; 
+		parents = new ArrayList<Node>();
+		colour = 0;
+		counter = 0;
 	} 
 }
 
-public class Lca {
+public class test {
 
 	static Node root;
 	
-		static Node lca(Node root, int n1,int n2)
-		{
 
-			if(root==null)
-				return null;
-
-		if(root.data==n1 || root.data==n2)
-			if(root.data==n1 || root.data==n2)
-            return root;
+        static void colour(Node search) {
             
-
-
+            search.parents.get(0).colour++;
+            return;
+            
+           /* if (search.parents == null) {
+                search.colour = 7;
+                return;
+            }
+            else{
+                for (int i = 0; i >= search.parents.size(); i++) {
+                    colour(search.parents.get(i));
+                    return;
+                }
+            }
+            System.out.println("fail");
+            return;*/
+            
         }
-    }
+
+        public static void main(String[] args) {
+
+		test tree = new test();
+
+		Node one = new Node(1);
+		Node two = new Node(2);
+		Node three = new Node(3);
+		Node four = new Node(4);
+		Node five = new Node(5);
+
+		tree.root = one;
+		two.parents.add(root); 
+		three.parents.add(root); 
+		four.parents.add(root);
+
+        five.parents.add(three);
+        five.parents.add(root);
+        
+        colour(five);
+        System.out.println("root colour: "+ three.colour);
+        }    
+}
